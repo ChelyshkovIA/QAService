b29959524912ca5e3b480b4894c10444a4959039
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-03-23 13:50:14 +0000

    init: fiori-groups:
    initial commit

---
+++ fiori-elements-apps/fiori-groups/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("fiorigroups.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
