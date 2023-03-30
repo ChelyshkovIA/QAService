2350c12efbed1e1851c1ff2624f1a269ff46476c
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-03-30 14:33:56 +0000

    feat: fiori-topics: Object Page:
    * added Controller extension to fix questions and groups tables selection
    mode (enabled only in edit mode);

--- fiori-elements-apps/fiori-topics/webapp/ext/controller/TopicDetailsControllerExt.controller.js
+++ fiori-elements-apps/fiori-topics/webapp/ext/controller/TopicDetailsControllerExt.controller.js
@@ -1,7 +1,39 @@
 sap.ui.define([
 
 ], function() {
+    const sQuestionsTableId = "fioritopics::sap.suite.ui.generic.template.ObjectPage.view.Details::Topics--questions::com.sap.vocabularies.UI.v1.LineItem::QuestionsLineItem::responsiveTable";
+    const sGroupsTableId = "fioritopics::sap.suite.ui.generic.template.ObjectPage.view.Details::Topics--groups::com.sap.vocabularies.UI.v1.LineItem::GroupsLineItem::gridTable";
+    
     return {
-        onInit() {}
+        onInit() {
+            const oQuestionsTable = this._getQuestionsTable();
+            const oGroupsTable = this._getGroupsTable();
+
+            oGroupsTable.removeAllPlugins();
+
+            this._bindTableProperty(oQuestionsTable, "mode", "SingleSelectLeft", "None");
+            this._bindTableProperty(oGroupsTable, "selectionMode", "MultiToggle", "None");
+        },
+
+        _bindTableProperty(oTable, sPropertyName, sFirstValue, sSecondValue) {
+            oTable.bindProperty(sPropertyName, {
+                path: "ui>/editable",
+                formatter: function(sValue) {
+                    return sValue ? sFirstValue : sSecondValue
+                }
+            });
+        },
+
+        _getQuestionsTable() {
+            return this._byId(sQuestionsTableId);
+        },
+
+        _getGroupsTable() {
+            return this._byId(sGroupsTableId);
+        },
+
+        _byId(sId) {
+            return this.getView().byId(sId);
+        }
     };
 });
