8ae369b792003b7f65fc295294873e7f8284e4c9
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-04-13 20:47:48 +0000

    initial: fiori-overview-topic
    * created empty overview page app

---
+++ fiori-elements-apps/fiori-overview-topic/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("fiorioverviewtopic.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
