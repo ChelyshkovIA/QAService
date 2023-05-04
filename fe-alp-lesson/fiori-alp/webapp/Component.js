bf02cbf62fefae3c26a440c20943334d79314726
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-05-04 20:50:00 +0000

    init: analytical list page: Initial Commits: * added XML Annotations for the UI Content Part:   - UI.LineItems (Anaylytical Table);   - UI.Chart (Interactive Chart);   - UI.PresentationVariant (Connects Interatctive Chart and Table);

---
+++ fe-alp-lesson/fiori-alp/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("fiorialp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
