a06fbdb2052b9a5b884faf438294a6ab5498e369
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-06-14 13:36:10 +0000

    feat: fiori-questions-alp: * added List Navigation Extension life cycle method definition

--- fiori-elements-apps/fiori-questions-alp/webapp/ext/controller/ALPExt.controller.js
+++ fiori-elements-apps/fiori-questions-alp/webapp/ext/controller/ALPExt.controller.js
@@ -5,6 +5,23 @@ sap.ui.define([
         onRefreshAllPress() {
             const oComponent = this.getOwnerComponent();
             oComponent.getModel().refresh();
+        },
+
+        onListNavigationExtension(oEvent) {
+            const oNavigationController = this.extensionAPI.getNavigationController();
+            const oBindingContext = oEvent.getSource().getBindingContext();
+            const oObject = oBindingContext.getObject();
+
+            /** Some condition here according to oObject values*/
+            if (oObject.somePropValue) {
+                oNavigationController.navigateExternal("SemanticObject");
+            } else {
+                /** False for default navigation */
+                return false;
+            }
+
+            /** In case of first condition true will prevent default navigation */
+            return true;
         }
     };
 });
