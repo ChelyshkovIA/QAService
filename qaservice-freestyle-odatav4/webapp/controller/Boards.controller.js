de680e3b2b155b09b788e908bd0fe6ac14332c75
Ilya Chelyshkov <ilya.chalyshkou@leverx.com>
2023-10-17 16:16:43 +0000

    feat: defined Board page: * added flexible column layout; * defined topics section; * defined groupt section; * defined questions section; * defined answer section; * set up dealing with binding contexts for:   -   - topics;   - groups;

--- qaservice-freestyle-odatav4/webapp/controller/Boards.controller.js
+++ qaservice-freestyle-odatav4/webapp/controller/Boards.controller.js
@@ -1,9 +1,96 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("qaservicefreestyleodatav4.controller.Boards", {
-        onInit() {}
+        onInit() {
+            this._defineJSONModel();
+            this._bindQuestionsList("/Questions");
+        },
+
+        _defineJSONModel() {
+            const oModel = new JSONModel({
+                selectedQuestion: {
+                    selected: false,
+                    answer: ""
+                },
+
+                selectedTopic: {
+                    id: "",
+                    name: ""
+                },
+
+                selectedGroup: {
+                    id: "",
+                    name: ""
+                }
+            });
+
+            this.getView().setModel(oModel, "app");
+        },
+
+        onTopicSelect(oEvent) {
+            const oGroupsList = this._getGroupsList();
+            const oCxt = oEvent.getSource().getSelectedItem().getBindingContext();
+            const oTopic = oCxt.getObject();
+            oGroupsList.setBindingContext(oCxt);
+            this._setTopicName(oTopic.name);
+            this._getQuestionsList().setBindingContext(oCxt);
+            this._bindQuestionsList("questions");
+        },
+
+        onGroupSelect(oEvent) {
+            const oGroupItem = oEvent.getSource().getSelectedItem();
+            const oCtx = oGroupItem.getBindingContext();
+            const oGroup = oCtx.getObject();
+            const oQuestionsList = this._getQuestionsList();
+            oQuestionsList.setBindingContext(oCtx);
+            this._bindQuestionsList("questions");
+            this._setGroupName(oGroup.name);
+        },
+
+        onQuestionSelect(oEvent) {
+            const oQuestionItem = oEvent.getSource().getSelectedItem();
+            const oCtx = oQuestionItem.getBindingContext();
+            const oQuestion = oCtx.getObject();
+            this._setQuestionSelected(true);
+            this._setQuestionAnswer(oQuestion.text);
+        },
+        
+        formatHighlight(test) {
+            return `Indication0${++test}`;
+        },
+
+        _setQuestionSelected(isSelected) {
+            this.getView().getModel("app").setProperty("/selectedQuestion/selected", isSelected);
+        },
+
+        _setQuestionAnswer(sAnswer) {
+            this.getView().getModel("app").setProperty("/selectedQuestion/answer", sAnswer);
+        },
+
+        _setGroupName(sName) {
+            this.getView().getModel("app").setProperty("/selectedGroup/name", sName);
+        },
+        
+        _setTopicName(sName) {
+            this.getView().getModel("app").setProperty("/selectedTopic/name", sName);
+        },
+
+        _bindQuestionsList(sPath) {
+            const oList = this._getQuestionsList();
+            const oTemplate = oList.getBindingInfo("items").template;
+            oList.bindItems({path: sPath, template: oTemplate});
+        },
+
+        _getQuestionsList() {
+            return this.getView().byId("questionsList");
+        },
+
+        _getGroupsList() {
+            return this.getView().byId("groupsList");
+        }
     });
 });
